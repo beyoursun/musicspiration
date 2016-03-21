2809139afe3f9dd8eb0a4e51137dec8f55025d84
sunbinbin <sunbinbin@xunlei.com>
2016-03-21 20:07:45 +0800

    update uploa

--- app/controllers/mus.server.controller.js
+++ app/controllers/mus.server.controller.js
@@ -1,3 +1,25 @@
-exports.create = function (req, res) {
+var formidable = require('formidable'),
+    fs = require('fs');
+
+exports.create = function(req, res) {
+    var form = formidable.IncomingForm();
+    form.encode = 'utf-8';
+    form.uploadDir = 'public/img/upload/';
+    form.keepExtensions = true;
+    form.maxFieldsSize = 2 * 1024 * 1024;
+
+    form.parse(req, function(err, fields, files) {
+        console.log(err);
+        
+        if (err) {
+            return res.json({ error: err });
+        }
+        
+        console.log('form');
+        console.log(fields);
+        console.log(files);
+    });
+
+    console.log('body');
     console.log(req.body);
 };
